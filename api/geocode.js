7d273e43acad0b3ae089e6d15ec1fe17836093cf
NLCLee <167361825+NLCLee@users.noreply.github.com>
2026-01-05 20:45:59 +0900

    Add files via upload

--- api/geocode.js
+++ api/geocode.js
@@ -23,8 +23,8 @@ export default async function handler(req, res) {
 			{
 				method: "GET",
 				headers: {
-					NCP_API_KEY_ID: process.env.NCT_ID,
-					NCP_API_KEY: process.env.NCT_SECRET,
+					"X-NCP-APIGW-API-KEY-ID": process.env.NCT_ID,
+					"X-NCP-APIGW-API-KEY": process.env.NCT_SECRET,
 					Accept: "application/json",
 				},
 			}
